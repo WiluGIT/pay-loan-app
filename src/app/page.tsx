a297daefcaadfde6dbabd5994e75219902378b82
wolframowsky <wolframowsky@gmail.com>
2025-03-30 21:39:49 +0200

    Lint fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/app/components/Container";
 import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
 import Steps from "@/app/components/Steps";
-import WhyUs from "@/app/components/WhyUs";
+//import WhyUs from "@/app/components/WhyUs";
 import Lead from "./components/Lead";
 
 export default function Home() {
